d7769ada3dac29f7006c36d73d73815ac8ba80f7
github username <therealgracechoi@gmail.com>
2020-07-15 17:06:38 -0700

    added avg score method

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -7,4 +7,9 @@ class Restaurant < ApplicationRecord
         self.slug = name.parameterize
     end
 
+    def avg_score 
+        return 0 unless reviews.count.positive?
+        
+        reviews.average(:score).round(2).to_f
+    end
 end
