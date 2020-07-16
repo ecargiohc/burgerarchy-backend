bdb2821f8c86a6d02dc13168ce4d384ecb48b4b5
github username <therealgracechoi@gmail.com>
2020-07-15 17:08:08 -0700

    added other methods for restaurants controller

--- app/controllers/api/v1/restaurants_controller.rb
+++ app/controllers/api/v1/restaurants_controller.rb
@@ -8,4 +8,34 @@ class Api::V1::RestaurantsController < ApplicationController
     render json: @restaurants
   end
  
+   # GET /restaurants/1
+   def show
+    render json: @restaurant
+  end
+
+  # POST /restaurants
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+
+    if @restaurant.save
+      render json: @restaurant, status: :created, location: @restaurant
+    else
+      render json: @restaurant.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /restaurants/1
+  def update
+    if @restaurant.update(restaurant_params)
+      render json: @restaurant
+    else
+      render json: @restaurant.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /restaurants/1
+  def destroy
+    @restaurant.destroy
+  end
+
 end
