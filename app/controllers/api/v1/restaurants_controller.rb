79bb8b838d5b45008028d2a7ff325768fe39c371
github username <therealgracechoi@gmail.com>
2020-07-15 17:07:11 -0700

    added index method for restaurants

--- app/controllers/api/v1/restaurants_controller.rb
+++ app/controllers/api/v1/restaurants_controller.rb
@@ -1,5 +1,11 @@
 class Api::V1::RestaurantsController < ApplicationController
   before_action :set_restaurant, only: [:show, :update, :destroy]
 
+   # GET /restaurants
+   def index
+    @restaurants = Restaurant.all
+
+    render json: @restaurants
+  end
  
 end
